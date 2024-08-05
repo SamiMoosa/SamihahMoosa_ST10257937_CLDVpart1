22bde2523442c82fc6c43604b8c7d2e2381d0b92
Samihah Moosa <ST10257937@vcconnect.edu.za>
2024-08-05 13:39:04 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10257937cldv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
