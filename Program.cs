6c529bdf3625281fcf2ed874697fe03280c8959c
Samihah Moosa <ST10257937@vcconnect.edu.za>
2024-08-05 13:43:00 +0200

    Program class chanages

--- Program.cs
+++ Program.cs
@@ -1,36 +1,44 @@
+
 namespace ST10257937cldv
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+
+        public class Program
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            public static void Main(string[] args)
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                var builder = WebApplication.CreateBuilder(args);
+
+                // Add services to the container.
+                builder.Services.AddControllersWithViews();
+
+                // Register your custom services
+                builder.Services.AddSingleton<BlobService>();
+                builder.Services.AddSingleton<TableService>();
+                builder.Services.AddSingleton<QueueService>();
+                builder.Services.AddSingleton<FileService>();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (!app.Environment.IsDevelopment())
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
         }
     }
-}
